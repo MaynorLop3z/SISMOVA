672a94b10899fbf6c2652c645618f17e8484bcc8
Maynor Lopez <maynorlop3z@gmail.com>
2016-10-06 10:20:51 -0600

    bk :cofee:

---
+++ BackUpDB/Dump20161006/Estaciones_Arduino.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `Estaciones` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `Estaciones`;
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: Estaciones
+-- ------------------------------------------------------
+-- Server version	5.5.47-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Arduino`
+--
+
+DROP TABLE IF EXISTS `Arduino`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Arduino` (
+  `idArduino` int(11) NOT NULL AUTO_INCREMENT,
+  `NombreArduino` varchar(150) NOT NULL,
+  `Latitud` double DEFAULT NULL,
+  `Longitud` double DEFAULT NULL,
+  `Descripcion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idArduino`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Arduino`
+--
+
+LOCK TABLES `Arduino` WRITE;
+/*!40000 ALTER TABLE `Arduino` DISABLE KEYS */;
+INSERT INTO `Arduino` VALUES (1,'Arduino1GT08',13.971289,-89.574847,'Prueba de Arduino, año 2015'),(2,'ArduinoExpresiones',13.990424,-89.560908,'Arduino ubicado en el restaurante Expresion Cultural'),(3,'Arduino2GT08',14.0116813,-89.5466765,'Año 2015, grupo del ing Melvin'),(4,'ArduinoCO2GT05',14.028696,-89.5364774,'Año 2015, el unico equipo armado por todo el grupo');
+/*!40000 ALTER TABLE `Arduino` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-06 10:19:01
